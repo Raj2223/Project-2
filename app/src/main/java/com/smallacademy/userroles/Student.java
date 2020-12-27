ba1a3dac89b87c8d7e86251f91de00a77c1459f8
raj2223 <dobariyaraj2222@gmail.com>
2020-12-27 19:14:01 +0530

    First commit

---
+++ app/src/main/java/com/smallacademy/userroles/Student.java
@@ -0,0 +1,32 @@
+package com.smallacademy.userroles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+
+        Button logout = findViewById(R.id.logoutBtn);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+
+            }
+        });
+
+    }
+}
